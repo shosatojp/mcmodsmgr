2209f0c4c956dcd9a2187c927843ab85276e00c6
shosatojp <me@shosato.jp>
2021-10-08 13:01:43 +0900

    downgrade clap

--- src/cli.rs
+++ src/cli.rs
@@ -1,59 +1,66 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version};
 pub use clap::{App, AppSettings, Arg, ArgMatches};
 
-pub fn build_cli() -> App<'static> {
+pub fn build_cli() -> App<'static, 'static> {
     let app = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
         .arg(
-            Arg::new("version")
-                .short('v')
-                .long("version")
+            Arg::with_name("mcversion")
+                .short("v")
+                .long("mcversion")
+                .global(true)
                 .takes_value(true),
         )
         .arg(
-            Arg::new("modloader")
-                .short('l')
+            Arg::with_name("modloader")
+                .short("l")
                 .long("modloader")
-                .about("mod loader")
+                .help("mod loader")
                 .takes_value(true)
+                .global(true)
                 .possible_value("forge")
                 .possible_value("fabric"),
         )
         .arg(
-            Arg::new("full")
-                .short('F')
+            Arg::with_name("full")
+                .short("F")
                 .long("full")
                 .takes_value(false)
-                .about("show all files instead of listing only latest files for each version"),
+                .global(true)
+                .help("show all files instead of listing only latest files for each version"),
         )
         .subcommand(
             App::new("search")
                 .about("search mods from curseforge.com")
-                .arg(Arg::new("name").takes_value(true).required(true)),
+                .arg(Arg::with_name("name").takes_value(true).required(true)),
         )
         .subcommand(
             App::new("install")
                 .about("download and place mod file")
-                .arg(Arg::new("name").takes_value(true))
+                .arg(Arg::with_name("name").takes_value(true).required(true))
                 .arg(
-                    Arg::new("fileid")
-                        .short('f')
+                    Arg::with_name("fileid")
+                        .short("f")
                         .long("fileid")
-                        .validator(|e| e.parse::<usize>())
+                        .validator(|e| {
+                            e.parse::<usize>()
+                                .and(Ok(()))
+                                .or(Err("parse int error".to_string()))
+                        })
                         .takes_value(true),
                 )
                 .arg(
-                    Arg::new("filename")
-                        .short('n')
+                    Arg::with_name("filename")
+                        .short("n")
                         .long("filename")
                         .takes_value(true),
                 )
                 .arg(
-                    Arg::new("dir")
-                        .about("directory to place mod file(s)")
-                        .short('d')
+                    Arg::with_name("dir")
+                        .help("directory to place mod file(s)")
+                        .short("d")
                         .long("dir")
                         .takes_value(true),
                 ),
@@ -61,7 +68,7 @@ pub fn build_cli() -> App<'static> {
         .subcommand(
             App::new("describe")
                 .about("describe a mod and list available files")
-                .arg(Arg::new("name").takes_value(true).required(true)),
+                .arg(Arg::with_name("name").takes_value(true).required(true)),
         );
 
     app
--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,17 @@ pub use api::curseforge_types::{Addon, AddonFile};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut app = cli::build_cli();
-    let root_matches = app.get_matches_mut();
+    let app = cli::build_cli();
+    let root_matches = app.get_matches_safe()?;
 
     match root_matches.subcommand() {
-        Some(("search", matches)) => {
+        ("search", Some(matches)) => {
             commands::search(matches.value_of("name").unwrap()).await?;
         }
-        Some(("install", matches)) => {
+        ("install", Some(matches)) => {
             commands::install(
                 matches.value_of("name").unwrap(),
-                root_matches.value_of("version"),
+                root_matches.value_of("mcversion"),
                 root_matches.value_of("modloader"),
                 matches
                     .value_of("fileid")
@@ -39,20 +39,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             )
             .await?;
         }
-        Some(("describe", matches)) => {
+        ("describe", Some(matches)) => {
             commands::describe(
                 matches.value_of("name").unwrap(),
-                root_matches.value_of("version"),
+                root_matches.value_of("mcversion"),
                 root_matches.value_of("modloader"),
                 !root_matches.is_present("full"),
             )
             .await?;
         }
-        Some((_, _)) => {
+        (_, _) => {
             // unreachable
-        }
-        None => {
-            let _ = app.print_help();
         }
     }
     Ok(())
